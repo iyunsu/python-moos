8982f76c8a82ec76ad9fea308a5b52c1f4b25e65
iyunsu <r07525010@ntu.edu.tw>
2019-04-07 21:12:20 +0800

    heading.py

--- heading.py
+++ heading.py
@@ -16,40 +16,37 @@ class lsm9ds1MOOS(pymoos.comms):
     """
     def __init__(self, moos_community, moos_port):
         """Initiates MOOSComms, sets the callbacks and runs the loop"""
-        super(pongMOOS, self).__init__()
+        super(lsm9ds1MOOS, self).__init__()
         self.server = moos_community
         self.port = moos_port
-        self.name = 'lsm9ds1MOOS'
-        self.iter = 0
+        self.name = 'lsm9ds1'
 
         self.set_on_connect_callback(self.__on_connect)
-        self.set_on_mail_callback(self.__on_new_mail)
+#        self.set_on_mail_callback(self.__on_new_mail)
         self.run(self.server, self.port, self.name)
 
     def __on_connect(self):
         """OnConnect callback"""
         print("Connected to", self.server, self.port,
               "under the name ", self.name)
-        return self.register('PING', 0)
-
-    def __on_new_mail(self):
-        """OnNewMail callback"""
-        #for msg in self.fetch():
-        #    if msg.key() == 'PING':
-        #        self.iter += 1
-        self.notify('MY_HEADING', float(heading), -1)
         return True
+
+#    def __on_new_mail(self):
+#        """OnNewMail callback"""
+#        #for msg in self.fetch():
+#        #    if msg.key() == 'PING':
+#        #        self.iter += 1
+#        self.notify('MY_HEADING', float(heading), -1)
+#        return True
 
 
 
 def main():
-    #ponger = pongMOOS('localhost', 9001)
-
-    #while True:
-    #    time.sleep(1)
+    H = lsm9ds1MOOS('localhost', 9000)
     
     while(1):
         heading = ls.get_heading()
+        H.notify('MY_HEADING', float(heading), -1)
         time.sleep(0.01)
 
 if __name__ == "__main__":
